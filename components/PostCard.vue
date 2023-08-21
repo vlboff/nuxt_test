6e3c138c6ab6ecb054f95073791272b649f8b077
Uladzimir <vl.barbikov@gmail.com>
2023-08-21 19:49:10 +0300

    add table

--- components/PostCard.vue
+++ components/PostCard.vue
@@ -4,7 +4,8 @@
       <UBadge color="primary" variant="outline">#{{ post.id }} </UBadge>
       <h2 class="inline pl-4 font-semibold text-xl text-gray-900 dark:text-white leading-tight">{{ post.title }}</h2>
     </template>
-    <slot name='body'></slot>
+    <p>{{ post.body }}</p>
+    <em>user #{{ post.userId }}</em>
     <UContainer :ui="{base: 'flex gap-2 px-0 sm:px-0 lg:px-0'}">
       <UBadge v-for="tag in post.tags" :key="tag" color="gray">
         {{ tag }}
@@ -12,7 +13,12 @@
     </UContainer>
     <p>likes: {{ post.reactions }}</p>
     <template #footer>
-      <slot></slot>
+      <UContainer :ui="{base: 'flex flex-col gap-2'}">
+        <UCard v-for="comment in comments" :key="comment.id">
+          <strong>user #{{ comment.id }}:</strong>
+          <p>{{ comment.body }}</p>
+        </UCard>
+      </UContainer>
     </template>
   </UCard>
 </template>
@@ -20,17 +26,31 @@
 <script setup lang="ts">
 
 type Post = {
-  id: number
-  title: string
-  body: string
-  tags: string[]
-  userId: number
-  reactions: number
+  id: number,
+  title: string,
+  body: string,
+  tags: string[],
+  userId: number,
+  reactions: number,
+}
+
+type Comment = {
+  id: number,
+  body: string,
+  postId: number,
+  user: {
+    id: number,
+    username: string,
+  }
 }
 
 defineProps ({
   post: {
     type: Object as () => Post,
+    required: true
+  },
+  comments: {
+    type: Object as () => Comment[],
     required: true
   }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,24 @@
 <template>
-  <UContainer :ui="{base: 'grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4 mx-auto py-4'}">
-    <post-card v-for="post in posts" :key="post.id" :post="post">
-      <UButton block>
-        <NuxtLink :to="`${post.id}`" >Comments</NuxtLink>
-      </UButton>
-    </post-card>
+  <UContainer>
+    <UTable
+      :rows="posts?.posts"
+      :columns="columns"
+      :loading="pending"
+      :ui="{
+        tr: {
+          base: 'hover:bg-gray-50 dark:hover:bg-gray-800/50 cursor-pointer'
+        }
+      }"
+      @select="select"
+    >
+      <template #tags-data="{ row }">
+        <div class='flex gap-2'>
+          <UBadge v-for="tag in row.tags" :key="tag" color="gray">
+            {{ tag }}
+          </UBadge>
+        </div>
+      </template>
+    </UTable>
   </UContainer>
   <UPagination
     v-model="page"
@@ -20,20 +34,59 @@
       }
     }"
   />
+  <UModal v-model="isOpen">
+    <post-card :post="selectedPost" :comments="comments"></post-card>
+  </UModal>
 </template>
 
 <script setup>
 
-const posts = ref([]);
+const columns = [{
+  key: 'id',
+  label: 'ID'
+}, {
+  key: 'title',
+  label: 'Title'
+}, {
+  key: 'tags',
+  label: 'Tags'
+}, {
+  key: 'reactions',
+  label: 'Likes'
+}];
+
 const page = ref(1);
 const pageTotal = ref(0);
 const pageCount = 30;
+const selectedPost = ref(null);
+const comments = ref([]);
+const isOpen = ref(false);
 
-useFetch(async () => {
-  const response = await fetch(`https://dummyjson.com/posts?limit=${pageCount}&skip=${(page.value - 1) * pageCount}`).then((res) => res.json());
-  posts.value = response.posts;
-  pageTotal.value = response.total;
-})
+const { data: posts, pending } = await useAsyncData('posts',
+  async () => {
+    const response = await $fetch(`https://dummyjson.com/posts?limit=${pageCount}&skip=${(page.value - 1) * pageCount}`);
+    pageTotal.value = response.total;
+    return response;
+  },
+  {
+    watch: [page],
+  }
+);
 
+const select = async (row) => {
+  const index = posts.value.posts.find((item) => item.id === row.id);
+  if (index === -1) {
+    selectedPost.value = null;
+  } else {
+    selectedPost.value = row;
+  };
+
+  const { data } = await useFetch(`https://dummyjson.com/posts/${selectedPost.value.id}/comments`, {
+    pick: ['comments']
+  })
+
+  comments.value = data.value.comments;
+  isOpen.value = true;
+}
 </script>
 
